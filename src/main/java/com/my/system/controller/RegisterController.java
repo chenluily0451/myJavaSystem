4bc7c61731e8cd5d5885241f2982048ec2dcdb0c
chenluily0451 <chenluily@163.com>
2017-11-27 15:56:43 +0800

    静态文件添加

--- src/main/java/com/my/system/controller/RegisterController.java
+++ src/main/java/com/my/system/controller/RegisterController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by chenlu on 2017/11/24.
  */
 @Controller
-public class LoginController {
-    @RequestMapping(value = "/web/login")
+public class RegisterController {
+    @RequestMapping(value = "/web/register")
     public String login() {
-        return "login/login";
+        return "register/register";
     }
 
-    @RequestMapping(value = "/web/login/success")
+    @RequestMapping(value = "/web/register/success")
     public String loginSuccess(String username, String password) {
         System.out.println("userName is " + username);
         System.out.println("password is " + password);
 
-        return "login/loginSuccess";
+        return "register/registerSuccess";
     }
 }
